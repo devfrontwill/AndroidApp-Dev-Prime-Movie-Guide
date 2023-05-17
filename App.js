8fa3404b0b49fd6eb33cc76857014f84cba2fe02
Wiliam Roza <devfrontwill@gmail.com>
2023-05-16 22:32:04 -0300

    criado toda estrutura necessaria para iniciar o desenvolvimento

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(){
+  return(
+    <View>
+    <Text>MEU APP DE FILMES</Text>
+  </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
