4b884202b9212a1178f0d826350c469ecd88c951
Wiliam Roza <devfrontwill@gmail.com>
2023-05-17 23:46:27 -0300

    Criado componente Home,Movie, feito a estilização com styled components/native

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, } from 'react-native';
 
-function Home(){
-    return(
-        <View>
-            <Text>TELA HOME</Text>
-        </View>
+import { Container, SearchContainer, Input, SearchButton } from './styles';
+
+import Header from '../../components/Header';
+import { Feather } from '@expo/vector-icons';
+
+
+function Home() {
+    return (
+        <Container>
+            <Header title="React Prime" />
+
+            <SearchContainer>
+                <Input 
+                placeholder="Ex Vingadores"
+                placeholderTextColor="#ddd"
+                />
+
+                <SearchButton>
+                    <Feather name='search' size={30} color="#FFF" />
+                </SearchButton>
+
+            </SearchContainer>
+            
+        </Container>
     )
 }
 
