0d4b1b93f5645061d5a3988f36affec78e441367
Wiliam Roza <devfrontwill@gmail.com>
2023-05-28 17:40:13 -0300

    FEAT

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,10 +1,99 @@
-import React from "react";
-import { Container, Title } from './styles';
+import React, { useState, useEffect } from "react";
+import { Container, Header, HeaderButton, Banner, ButtonLink, Title, ContentArea, Rate } from './styles';
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import api, { key } from '../../services/api';
+import Stars from 'react-native-stars';
 
-function Detail(){
-    return(
+
+function Detail() {
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        let isActive = true;
+
+        async function getMovie() {
+            const response = await api.get(`/movie/${route.params?.id}`, {
+                params: {
+                    api_key: key,
+                    language: 'pt-BR'
+                }
+            })
+                .catch((err) => {
+                    console.log(err)
+                })
+
+            if (isActive) {
+                setMovie(response.data);
+            }
+        }
+        if (isActive) {
+
+            getMovie();
+        }
+
+        return () => {
+            isActive = false;
+        }
+
+    }, [])
+
+
+    return (
         <Container>
-            <Title> TESTE TESTE TESTE </Title>
+            <Header>
+                <HeaderButton activeOpacity={0.8} onPress={() => navigation.goBack()} >
+                    <Feather
+                        name="arrow-left"
+                        size={28}
+                        color="#FFF"
+                    />
+                </HeaderButton>
+
+                <HeaderButton>
+                    <Ionicons
+                        name="bookmark"
+                        size={28}
+                        color="#FFF"
+                    />
+                </HeaderButton>
+
+            </Header>
+
+            <Banner
+                resizeMethod="resize"
+                source={{ uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}` }}
+            />
+
+            <ButtonLink>
+                <Feather
+                    name="link"
+                    size={24}
+                    color="#FFF"
+                />
+            </ButtonLink>
+
+            <Title
+                numberOfLines={2}>{movie.title}
+            </Title>
+
+            <ContentArea>
+                <Stars
+                    default={movie.vote_average}
+                    count={10}
+                    half={true}
+                    starSize={20}
+                    fullStar={ <Ionicons name="md-star" size={24} color="#E7A74e" />}
+                    emptyStar={ <Ionicons name="md-star-outline" size={24} color="#E7A74e" />}
+                    halfStar={ <Ionicons name="md-star-half" size={24} color="#E7A74e" />}
+                    disabled={true}
+                />
+                <Rate> {movie.vote_average}/10 </Rate>
+            </ContentArea>
+
         </Container>
     )
 }
