25f898a24198498af6da8e1261dddb864efa56c0
Wiliam Roza <devfrontwill@gmail.com>
2023-06-01 15:35:07 -0300

    feat

--- src/components/SearchItem/index.js
+++ src/components/SearchItem/index.js
@@ -1,32 +1,32 @@
 import React from "react";
-import {View,Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
 
-import { Container, Banner, Title,RateContainer, Rate } from './styles';
+import { Container, Banner, Title, RateContainer, Rate } from './styles';
 
-function SearchItem({ data, navigatePage }){
+function SearchItem({ data, navigatePage }) {
 
-    function detailMovie(){
-        if(data.release_date === ''){
-            alert('Filme ainda sem data de lançamento!');
+    function detailMovie() {
+        if (data.release_date === '') {
+            alert('Filme com data de lançamento não definida!');
             return;
         }
 
         navigatePage(data);
     }
 
-    return(
+    return (
         <Container activeOpacity={0.7} onPress={detailMovie} >
             {data?.poster_path ? (
                 <Banner
-                 resizeMethod="resize"
-                 source={{ uri: `https://image.tmdb.org/t/p/original/${data?.poster_path}`}}
+                    resizeMethod="resize"
+                    source={{ uri: `https://image.tmdb.org/t/p/original/${data?.poster_path}` }}
                 />
             ) : (
                 <Banner
-                 resizeMethod="resize"
-                 source={ require('../../assets/semfoto.png')}
+                    resizeMethod="resize"
+                    source={require('../../assets/semfoto.png')}
                 />
             )}
 
